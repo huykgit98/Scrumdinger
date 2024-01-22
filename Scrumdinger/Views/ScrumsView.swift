a066d06112429aec68f2afddc51472a344b816c1
Negan <huykgit98@gmail.com>
2024-01-22 18:47:02 +0700

    Implement ScrumsView that displays a list of daily scrum

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,29 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Negan on 22/01/2024.
+//
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+
+        }
+    }
+    
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
